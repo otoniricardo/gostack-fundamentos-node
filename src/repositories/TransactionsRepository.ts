ec93dcbb18e4a79350debf8fd4c73d4dfba1c346
otoniricardo <otoniricardo@gmail.com>
2020-06-18 20:44:47 -0300

    implementa soluções

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -15,14 +22,39 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const incomeTransactions = this.transactions.filter(
+      ({ type }) => type === 'income',
+    );
+
+    const outcomeTransactions = this.transactions.filter(
+      ({ type }) => type === 'outcome',
+    );
+
+    const income = incomeTransactions.reduce((acc, curr) => {
+      acc += curr.value;
+      return acc;
+    }, 0);
+
+    const outcome = outcomeTransactions.reduce((acc, curr) => {
+      acc += curr.value;
+      return acc;
+    }, 0);
+
+    return { income, outcome, total: income - outcome };
   }
 
-  public create(): Transaction {
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
